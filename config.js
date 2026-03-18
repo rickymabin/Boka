fe72f7cc8214946e288ef2eba051d80c46eafed3
rickymabin <154897923+rickymabin@users.noreply.github.com>
2026-03-18 21:54:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   LIFF_ID: "2009512164-DC24Cr80",
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbzmAi1YuZyuhVHmV67HdUpDuyn-wjqzG1jfLN09OPtf5A_TkPb3KrwgnMDnww0V5D_HIg/exec"
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbzyOP9EnrQZnDDNMpCFpvA7vrMCtGDc-CDwyrMYYwqXEjXj9m6FPtdZWW-Yt5TTHJOUeg/exec"
 };
