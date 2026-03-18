233aac6ce916cee3d4a08c97e8100e5408b5267d
rickymabin <154897923+rickymabin@users.noreply.github.com>
2026-03-18 16:59:12 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+把 frontend 部署到靜態網站：
+- GitHub Pages
+- Netlify
+- Vercel
+- Render Static Site
+
+部署後，修改 config.js：
+1. LIFF_ID
+2. GAS_API_URL
---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  LIFF_ID: "2009512164-DC24Cr80",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbzpGEwQkqOgquyLu51NwHwbAJ8AGT9RXKXy2rpXcDot7NGcoxWWTvUFqVses_BFtP-IzA/exec"
+};
